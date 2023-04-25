0ede5c21f2a664d24634e066a82a356f31091ef9
bsdaoquang <bsdaoquang@gmail.com>
2023-04-25 23:38:22 +0700

    handle navigation with notification

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import { Linking } from 'react-native';
 
 messaging().setBackgroundMessageHandler(async message =>
 {
-  // navigation deeplink to screen when user click notification
-  console.log(`Background message: ${message}`);
+  console.log(message);
+});
+
+messaging().onNotificationOpenedApp(messaging =>
+{
+  Linking.openURL(`myapp://app/${messaging.data.screen}`);
 });
 
 
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -2,7 +2,9 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {globalStyle} from '../styles/globalStyles';
 
-const ProfileScreen = () => {
+const ProfileScreen = ({route, navigation}: any) => {
+  console.log(route.params);
+
   return (
     <View style={globalStyle.container}>
       <Text>Profile screen</Text>
